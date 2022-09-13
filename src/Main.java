0ab71383d384df266306b6525ee3901ca7d84a79
bilal karahan <bilalkarahan309@gmail.com>
2022-09-13 14:16:38 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Yıl Giriniz :");
+        /*int yil = inp.nextInt();
+        int kalan3 = yil % 100, kalan2 = yil % 400, kalan1 = yil % 4;
+        */int yil = scan.nextInt();
+
+        if(((yil % 4 == 0) && (yil % 100 != 0)) || (yil % 400 == 0)){
+            System.out.println(yil + " artık yıldır.");
+        }else {
+            System.out.println(yil + " artık yıl değildir.");
+        }
+ /*       if (kalan1 == 0) {
+            if (kalan3 != 0) {
+                System.out.println(yil + "bir artık yıldır!");
+
+            } else if (kalan2 == 0) {
+                System.out.println(yil + "bir artık yıldır!");
+            } else {
+                System.out.println(yil + "bir artık yıl değildir !");
+            }
+
+       }
+  */
     }
+
+
 }
